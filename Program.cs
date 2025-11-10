bc8e8e1c0c8694683a5be0cfa473cb8c176412c1
Tair <dzum@gmail.com>
2025-11-10 09:32:23 +0300

    Изменение файла Program

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ namespace lab_7
     public class Program{
         public static void Main()
         {
-            Console.WriteLine("Hello, GitHub!");
+            Console.WriteLine("Welcome to my project!");
         } 
     }
 }
